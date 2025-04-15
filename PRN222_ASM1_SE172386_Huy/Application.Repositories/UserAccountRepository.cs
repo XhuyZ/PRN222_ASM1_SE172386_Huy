9b4d5620700ea8cfd68de750c55311ef0846d44f
XhuyZ <huylovengoc35@gmail.com>
2025-04-15 10:11:14 +0700

    Getuseraccount function

--- PRN222_ASM1_SE172386_Huy/Application.Repositories/UserAccountRepository.cs
+++ PRN222_ASM1_SE172386_Huy/Application.Repositories/UserAccountRepository.cs
@@ -4,6 +4,15 @@ namespace Application.Repositories
 {
     public class UserAccountRepository : GenericRepository<System_UserAccount>
     {
+        public UserAccountRepository()
+        {
+
+        }
+        public async Task<System_UserAccount> GetUserAccount(string username, string password)
+        {
+            return await _context.System_UserAccounts.FirstOrDefault(u => u.UserName == username && u.Password == password && u.IsActive == true);
+
+        }
 
     }
 }
