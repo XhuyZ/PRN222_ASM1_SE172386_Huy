1221c2c5d3d2d433f9f0679b7e8ebd9c2b0337b8
XhuyZ <huylovengoc35@gmail.com>
2025-04-15 08:44:36 +0700

    init

---
+++ PRN222_ASM1_SE172386_Huy/Application.Repositories/Context/MyDbContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Application.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repositories.Context;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<System_UserAccount> System_UserAccounts { get; set; }
+
+    public virtual DbSet<Vaccine> Vaccines { get; set; }
+
+    public virtual DbSet<VaccinePackage> VaccinePackages { get; set; }
+
+    public virtual DbSet<VaccinePackageDetail> VaccinePackageDetails { get; set; }
+
+    public static string GetConnectionString(string connectionStringName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string connectionString = config.GetConnectionString(connectionStringName);
+        return connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Vaccine>(entity =>
+        {
+            entity.HasKey(e => e.VaccineId).HasName("PK__Vaccines__45DC6889121D0127");
+
+            entity.Property(e => e.VaccineId).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<VaccinePackage>(entity =>
+        {
+            entity.HasKey(e => e.VaccinePackageID).HasName("PK__VaccineP__BEA96C823A1F8BBE");
+
+            entity.Property(e => e.VaccinePackageID).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<VaccinePackageDetail>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__VaccineP__3214EC07FE918F11");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.Vaccine).WithMany(p => p.VaccinePackageDetails).HasConstraintName("FK__VaccinePa__Vacci__60A75C0F");
+
+            entity.HasOne(d => d.VaccinePackage).WithMany(p => p.VaccinePackageDetails).HasConstraintName("FK__VaccinePa__Vacci__619B8048");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PRN222_ASM1_SE172386_Huy/Application.Repositories/Models/System_UserAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repositories.Models;
+
+[Table("System.UserAccount")]
+public partial class System_UserAccount
+{
+    [Key]
+    public int UserAccountID { get; set; }
+
+    [StringLength(50)]
+    public string UserName { get; set; } = null!;
+
+    [StringLength(100)]
+    public string Password { get; set; } = null!;
+
+    [StringLength(100)]
+    public string FullName { get; set; } = null!;
+
+    [StringLength(150)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(50)]
+    public string Phone { get; set; } = null!;
+
+    [StringLength(50)]
+    public string EmployeeCode { get; set; } = null!;
+
+    public int RoleId { get; set; }
+
+    [StringLength(50)]
+    public string? RequestCode { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedDate { get; set; }
+
+    [StringLength(50)]
+    public string? ApplicationCode { get; set; }
+
+    [StringLength(50)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? ModifiedDate { get; set; }
+
+    [StringLength(50)]
+    public string? ModifiedBy { get; set; }
+
+    public bool IsActive { get; set; }
+}
